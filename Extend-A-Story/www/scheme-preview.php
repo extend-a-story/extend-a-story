443849da443cff212463a917122e3de8ea44ef65
Jeff Weston <jjweston@gmail.com>
2012-08-04 19:02:33 -0700

    Updated formatting.

--- Extend-A-Story/www/scheme-preview.php
+++ Extend-A-Story/www/scheme-preview.php
@@ -26,85 +26,107 @@ http://www.sir-toby.com/extend-a-story/
 
 */
 
-  require( "ExtendAStory.php" );
+    require( "ExtendAStory.php" );
 
-  $scheme = $_POST[ "scheme" ];
+    $scheme = $_POST[ "scheme" ];
 
-  $error = "";
-  $fatal = false;
+    $error = "";
+    $fatal = false;
 
-  $scheme = ( int ) $scheme;
+    $scheme = (int) $scheme;
 
-  if ( $scheme == 0 )
-    $scheme = 1;
-
-  // Connect to the database.
-  if ( empty( $error ) )
-    connectToDatabase( $error, $fatal );
-
-  if ( empty( $error ) )
-    getSessionAndUserIDs( $error, $fatal, $sessionID, $userID );
-
-  if ( empty( $error ) )
-  {
-    $storyName = getStringValue( $error, $fatal, "StoryName" );
-    $siteName  = getStringValue( $error, $fatal, "SiteName"  );
-    $storyHome = getStringValue( $error, $fatal, "StoryHome" );
-    $siteHome  = getStringValue( $error, $fatal, "SiteHome"  );
-  }
-
-  if ( empty( $error ) )
-  {
-    $result = mysql_query( "select SchemeName, bgcolor, text, link, vlink, alink, background, " .
-                           "UncreatedLink, CreatedLink, BackLinkedLink from Scheme where SchemeID = " . $scheme );
-    if ( ! $result )
+    if ( $scheme == 0 )
     {
-      $error .= "Problem retrieving the scheme from the database.<BR>";
-      $fatal = true;
+        $scheme = 1;
     }
-    else
+
+    // connect to the database
+    if ( empty( $error ))
     {
-      $row = mysql_fetch_row( $result );
-      if ( ! $row )
-      {
-        $error .= "Problem fetching scheme row from the database.<BR>";
-        $fatal = true;
-      }
-      else
-      {
-        $schemeName     = $row[ 0 ];
-        $bgcolor        = $row[ 1 ];
-        $text           = $row[ 2 ];
-        $link           = $row[ 3 ];
-        $vlink          = $row[ 4 ];
-        $alink          = $row[ 5 ];
-        $background     = $row[ 6 ];
-        $uncreatedLink  = $row[ 7 ];
-        $createdLink    = $row[ 8 ];
-        $backLinkedLink = $row[ 9 ];
-
-        $body = "<BODY BGCOLOR=\"" . $bgcolor . "\" " .
-                      "TEXT=\""    . $text    . "\" " .
-                      "LINK=\""    . $link    . "\" " .
-                      "VLINK=\""   . $vlink   . "\" " .
-                      "ALINK=\""   . $alink   . "\""  .
-                      ( empty( $background ) ? ">" : " BACKGROUND=\"" . $background . "\">" );
-      }
+        connectToDatabase( $error, $fatal );
     }
-  }
 
-  if ( empty( $error ) )
-  {
-    $result = mysql_query( "select SchemeID, SchemeName from Scheme" );
-    if ( ! $result )
+    if ( empty( $error ))
     {
-      $error .= "Problem retrieving the list of schemes from the database.<BR>";
-      $fatal = true;
+        getSessionAndUserIDs( $error, $fatal, $sessionID, $userID );
     }
-  }
 
-  if ( ! empty( $error ) )
-    displayError( $error, $fatal );
+    if ( empty( $error ))
+    {
+        $storyName = getStringValue( $error, $fatal, "StoryName" );
+        $siteName  = getStringValue( $error, $fatal, "SiteName"  );
+        $storyHome = getStringValue( $error, $fatal, "StoryHome" );
+        $siteHome  = getStringValue( $error, $fatal, "SiteHome"  );
+    }
+
+    if ( empty( $error ))
+    {
+        $result = mysql_query( "SELECT SchemeName, " .
+                                      "bgcolor, " .
+                                      "text, " .
+                                      "link, " .
+                                      "vlink, " .
+                                      "alink, " .
+                                      "background, " .
+                                      "UncreatedLink, " .
+                                      "CreatedLink, " .
+                                      "BackLinkedLink " .
+                                 "FROM Scheme " .
+                                "WHERE SchemeID = " . $scheme );
+
+        if ( ! $result )
+        {
+            $error .= "Problem retrieving the scheme from the database.<BR>";
+            $fatal = true;
+        }
+        else
+        {
+            $row = mysql_fetch_row( $result );
+
+            if ( ! $row )
+            {
+                $error .= "Problem fetching scheme row from the database.<BR>";
+                $fatal = true;
+            }
+            else
+            {
+                $schemeName     = $row[ 0 ];
+                $bgcolor        = $row[ 1 ];
+                $text           = $row[ 2 ];
+                $link           = $row[ 3 ];
+                $vlink          = $row[ 4 ];
+                $alink          = $row[ 5 ];
+                $background     = $row[ 6 ];
+                $uncreatedLink  = $row[ 7 ];
+                $createdLink    = $row[ 8 ];
+                $backLinkedLink = $row[ 9 ];
+
+                $body = "<BODY BGCOLOR=\"" . $bgcolor . "\" " .
+                              "TEXT=\""    . $text    . "\" " .
+                              "LINK=\""    . $link    . "\" " .
+                              "VLINK=\""   . $vlink   . "\" " .
+                              "ALINK=\""   . $alink   . "\""  .
+                              ( empty( $background ) ? ">" :
+                                                       " BACKGROUND=\"" . $background . "\">" );
+            }
+        }
+    }
+
+    if ( empty( $error ))
+    {
+        $result = mysql_query( "select SchemeID, SchemeName from Scheme" );
+
+        if ( ! $result )
+        {
+            $error .= "Problem retrieving the list of schemes from the database.<BR>";
+            $fatal = true;
+        }
+    }
+
+    if ( ! empty( $error ))
+    {
+        displayError( $error, $fatal );
+    }
 
 ?>
 
@@ -117,39 +139,55 @@ http://www.sir-toby.com/extend-a-story/
 <H2>Now Previewing: <?php echo( $schemeName ); ?></H2>
 
 <TABLE WIDTH="500">
-  <TR>
-    <TD>
+    <TR>
+        <TD>
 This special page allows you to preview the color schemes available to you
 when creating episodes. Since many of the schemes would interfere with the
 creation interface, this preview is loaded in a seperate window.
 <P>
 <OL>
-<LI><IMG SRC="<?php echo( $uncreatedLink  ); ?>"><A HREF="#">This is an example of an uncreated link.</A></LI>
-<LI><IMG SRC="<?php echo( $createdLink    ); ?>"><A HREF="#">This is an example of a created link.</A></LI>
-<LI><IMG SRC="<?php echo( $backLinkedLink ); ?>"><A HREF="#">This is an example of a back linked link.</A></LI>
+<LI>
+    <IMG SRC="<?php echo( $uncreatedLink  ); ?>">
+    <A HREF="#">This is an example of an uncreated link.</A>
+</LI>
+<LI>
+    <IMG SRC="<?php echo( $createdLink    ); ?>">
+    <A HREF="#">This is an example of a created link.</A>
+</LI>
+<LI>
+    <IMG SRC="<?php echo( $backLinkedLink ); ?>">
+    <A HREF="#">This is an example of a back linked link.</A>
+</LI>
 </OL>
 <P>
 <FORM METHOD="POST">
 Select another scheme to preview:<BR>
 <SELECT NAME="scheme">
+
 <?php
 
-  for ( $i = 0; $i < mysql_num_rows( $result ); $i++ )
-  {
-    $row = mysql_fetch_row( $result );
+    for ( $i = 0; $i < mysql_num_rows( $result ); $i++ )
+    {
+        $row = mysql_fetch_row( $result );
+        $selected = ( $scheme == $row[ 0 ] ) ? " SELECTED" : "";
 
 ?>
-<OPTION VALUE="<?php echo( $row[ 0 ] ); ?>"<?php echo( $scheme == $row[ 0 ] ? " SELECTED" : "" ); ?>><?php echo( $row[ 1 ] );?></OPTION>
+
+<OPTION VALUE="<?php echo( $row[ 0 ] ); ?>"<?php echo( $selected ); ?>>
+    <?php echo( $row[ 1 ] );?>
+</OPTION>
+
 <?php
 
-  }
+    }
 
 ?>
+
 </SELECT>
 <INPUT TYPE="submit" VALUE="Go">
 </FORM>
-    </TD>
-  </TR>
+        </TD>
+    </TR>
 </TABLE>
 
 </CENTER>
