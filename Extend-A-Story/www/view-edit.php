5386935745bd1f7fcf095634c7a76423ae63a280
Jeff Weston <jjweston@gmail.com>
2012-08-04 21:12:02 -0700

    Updated formatting.

--- Extend-A-Story/www/view-edit.php
+++ Extend-A-Story/www/view-edit.php
@@ -26,50 +26,70 @@ http://www.sir-toby.com/extend-a-story/
 
 */
 
-  require( "ExtendAStory.php" );
+    require( "ExtendAStory.php" );
 
-  $error = "";
-  $fatal = false;
+    $error = "";
+    $fatal = false;
 
-  $episodeEditLogID = $_GET[ "episodeEditLogID" ];
+    $episodeEditLogID = $_GET[ "episodeEditLogID" ];
 
-  $episodeEditLogID = ( int ) $episodeEditLogID;
+    $episodeEditLogID = (int) $episodeEditLogID;
 
-  if ( $episodeEditLogID == 0 )
-    $episodeEditLogID = 1;
-
-  // Connect to the database.
-  if ( empty( $error ) )
-    connectToDatabase( $error, $fatal );
-
-  if ( empty( $error ) )
-    getSessionAndUserIDs( $error, $fatal, $sessionID, $userID );
-
-  if ( empty( $error ) )
-  {
-    $storyName = getStringValue( $error, $fatal, "StoryName" );
-    $siteName  = getStringValue( $error, $fatal, "SiteName"  );
-    $storyHome = getStringValue( $error, $fatal, "StoryHome" );
-    $siteHome  = getStringValue( $error, $fatal, "SiteHome"  );
-  }
-
-  if ( empty( $error ) )
-  {
-    $result = mysql_query( "select EpisodeID, SchemeID, ImageID, IsLinkable, IsExtendable, AuthorMailto, AuthorNotify, " .
-                                  "Title, Text, AuthorName, AuthorEmail, EditDate from EpisodeEditLog " .
-                           "where EpisodeEditLogID = " . $episodeEditLogID );
-    if ( ! $result )
+    if ( $episodeEditLogID == 0 )
     {
-      $error .= "Problem retrieving episode edit log record from the database.<BR>";
-      $fatal = true;
+        $episodeEditLogID = 1;
     }
-    else
+
+    // connect to the database
+    if ( empty( $error ))
     {
-      $row = mysql_fetch_row( $result );
-      if ( ! $row )
-      {
+        connectToDatabase( $error, $fatal );
+    }
+
+    if ( empty( $error ))
+    {
+        getSessionAndUserIDs( $error, $fatal, $sessionID, $userID );
+    }
+
+    if ( empty( $error ))
+    {
+        $storyName = getStringValue( $error, $fatal, "StoryName" );
+        $siteName  = getStringValue( $error, $fatal, "SiteName"  );
+        $storyHome = getStringValue( $error, $fatal, "StoryHome" );
+        $siteHome  = getStringValue( $error, $fatal, "SiteHome"  );
+    }
+
+    if ( empty( $error ))
+    {
+        $result = mysql_query( "SELECT EpisodeID, " .
+                                      "SchemeID, " .
+                                      "ImageID, " .
+                                      "IsLinkable, " .
+                                      "IsExtendable, " .
+                                      "AuthorMailto, " .
+                                      "AuthorNotify, " .
+                                      "Title, " .
+                                      "Text, " .
+                                      "AuthorName, " .
+                                      "AuthorEmail, " .
+                                      "EditDate " .
+                                 "FROM EpisodeEditLog " .
+                                "WHERE EpisodeEditLogID = " . $episodeEditLogID );
+
+        if ( ! $result )
+        {
+            $error .= "Problem retrieving episode edit log record from the database.<BR>";
+            $fatal = true;
+        }
+        else
+        {
+            $row = mysql_fetch_row( $result );
+
+            if ( ! $row )
+            {
 
 ?>
+
 <HTML><HEAD>
 <TITLE>Story Error - Episode Edit Log <?php echo( $episodeEditLogID ); ?> Not Found</TITLE>
 </HEAD><BODY>
@@ -85,30 +105,31 @@ http://www.sir-toby.com/extend-a-story/
 <?php require( "footer.php" ); ?>
 
 </BODY></HTML>
+
 <?php
 
-        exit;
-      }
+                exit;
+            }
 
-      $episode      = $row[ 0  ];
-      $scheme       = $row[ 1  ];
-      $image        = $row[ 2  ];
-      $isLinkable   = $row[ 3  ];
-      $isExtendable = $row[ 4  ];
-      $authorMailto = $row[ 5  ];
-      $authorNotify = $row[ 6  ];
-      $title        = $row[ 7  ];
-      $text         = $row[ 8  ];
-      $authorName   = $row[ 9  ];
-      $authorEmail  = $row[ 10 ];
-      $editDate     = $row[ 11 ];
+            $episode      = $row[ 0  ];
+            $scheme       = $row[ 1  ];
+            $image        = $row[ 2  ];
+            $isLinkable   = $row[ 3  ];
+            $isExtendable = $row[ 4  ];
+            $authorMailto = $row[ 5  ];
+            $authorNotify = $row[ 6  ];
+            $title        = $row[ 7  ];
+            $text         = $row[ 8  ];
+            $authorName   = $row[ 9  ];
+            $authorEmail  = $row[ 10 ];
+            $editDate     = $row[ 11 ];
+        }
     }
-  }
 
-  $canEdit = canEditEpisode( $sessionID, $userID, $episode );
+    $canEdit = canEditEpisode( $sessionID, $userID, $episode );
 
-  if ( ! $canEdit )
-  {
+    if ( ! $canEdit )
+    {
 
 ?>
 
@@ -120,13 +141,13 @@ http://www.sir-toby.com/extend-a-story/
 <H1>Edit Log - Error</H1>
 
 <TABLE WIDTH="500">
-  <TR>
-    <TD>
+    <TR>
+        <TD>
 You do not have permission to view this edit log.
 <P>
 <A HREF="read.php?episode=<?php echo( $episode ); ?>">Go Back</A>
-    </TD>
-  </TR>
+        </TD>
+    </TR>
 </TABLE>
 
 </CENTER>
@@ -137,141 +158,170 @@ You do not have permission to view this edit log.
 
 <?php
 
-    exit;
-  }
-
-  if ( empty( $error ) )
-  {
-    $result = mysql_query( "select Parent from Episode where EpisodeID = " . $episode );
-    if ( ! $result )
-    {
-      $error .= "Problem retrieving the episode from the database.<BR>";
-      $fatal = true;
+        exit;
     }
-    else
-    {
-      $row = mysql_fetch_row( $result );
-      if ( ! $row )
-      {
-        $error .= "Problem fetching episode row from the database.<BR>";
-        $fatal = true;
-      }
-      else
-      {
-        $parent = $row[ 0 ];
-      }
-    }
-  }
 
-  if ( empty( $error ) )
-  {
-    $result = mysql_query( "select count( * ) from Link where TargetEpisodeID = " . $episode );
-    if ( ! $result )
+    if ( empty( $error ))
     {
-      $error .= "Problem retrieving link count from the database.<BR>";
-      $fatal = true;
+        $result = mysql_query( "SELECT Parent FROM Episode WHERE EpisodeID = " . $episode );
+
+        if ( ! $result )
+        {
+            $error .= "Problem retrieving the episode from the database.<BR>";
+            $fatal = true;
+        }
+        else
+        {
+            $row = mysql_fetch_row( $result );
+
+            if ( ! $row )
+            {
+                $error .= "Problem fetching episode row from the database.<BR>";
+                $fatal = true;
+            }
+            else
+            {
+                $parent = $row[ 0 ];
+            }
+        }
     }
-    else
+
+    if ( empty( $error ))
     {
-      $row = mysql_fetch_row( $result );
-      if ( ! $row )
-      {
-        $error .= "Problem fetching link count row from the database.<BR>";
-        $fatal = true;
-      }
-      else
-      {
-        $linkCount = ( int ) $row[ 0 ];
-      }
+        $result = mysql_query( "SELECT COUNT( * ) FROM Link WHERE TargetEpisodeID = " . $episode );
+
+        if ( ! $result )
+        {
+            $error .= "Problem retrieving link count from the database.<BR>";
+            $fatal = true;
+        }
+        else
+        {
+            $row = mysql_fetch_row( $result );
+
+            if ( ! $row )
+            {
+                $error .= "Problem fetching link count row from the database.<BR>";
+                $fatal = true;
+            }
+            else
+            {
+                $linkCount = (int) $row[ 0 ];
+            }
+        }
     }
-  }
 
-  $title      = htmlentities( $title );
-  $text       = htmlentities( $text  );
-  $authorName = htmlentities( $authorName );
+    $title      = htmlentities( $title );
+    $text       = htmlentities( $text  );
+    $authorName = htmlentities( $authorName );
 
-  $text        = strtr( $text,        getEpisodeBodyTranslationTable(  ) );
-  $authorEmail = strtr( $authorEmail, getEmailAddressTranslationTable( ) );
+    $text        = strtr( $text,        getEpisodeBodyTranslationTable()  );
+    $authorEmail = strtr( $authorEmail, getEmailAddressTranslationTable() );
 
-  if ( empty( $error ) )
-  {
-    $result = mysql_query( "select bgcolor, text, link, vlink, alink, background, " .
-                           "UncreatedLink, CreatedLink, BackLinkedLink from Scheme where SchemeID = " . $scheme );
-    if ( ! $result )
+    if ( empty( $error ))
     {
-      $error .= "Problem retrieving the scheme from the database.<BR>";
-      $fatal = true;
-    }
-    else
-    {
-      $row = mysql_fetch_row( $result );
-      if ( ! $row )
-      {
-        $error .= "Problem fetching scheme row from the database.<BR>";
-        $fatal = true;
-      }
-      else
-      {
-        $bgcolorColor   = $row[ 0 ];
-        $textColor      = $row[ 1 ];
-        $linkColor      = $row[ 2 ];
-        $vlinkColor     = $row[ 3 ];
-        $alinkColor     = $row[ 4 ];
-        $background     = $row[ 5 ];
-        $uncreatedLink  = $row[ 6 ];
-        $createdLink    = $row[ 7 ];
-        $backLinkedLink = $row[ 8 ];
+        $result = mysql_query( "SELECT bgcolor, " .
+                                      "text, " .
+                                      "link, " .
+                                      "vlink, " .
+                                      "alink, " .
+                                      "background, " .
+                                      "UncreatedLink, " .
+                                      "CreatedLink, " .
+                                      "BackLinkedLink " .
+                                 "FROM Scheme " .
+                                "WHERE SchemeID = " . $scheme );
 
-        $body = "<BODY BGCOLOR=\"" . $bgcolorColor . "\" " .
-                      "TEXT=\""    . $textColor    . "\" " .
-                      "LINK=\""    . $linkColor    . "\" " .
-                      "VLINK=\""   . $vlinkColor   . "\" " .
-                      "ALINK=\""   . $alinkColor   . "\""  .
-                      ( empty( $background ) ? ">" : " BACKGROUND=\"" . $background . "\">" );
-      }
-    }
-  }
+        if ( ! $result )
+        {
+            $error .= "Problem retrieving the scheme from the database.<BR>";
+            $fatal = true;
+        }
+        else
+        {
+            $row = mysql_fetch_row( $result );
 
-  if ( ( empty( $error ) ) && ( $image != 0 ) )
-  {
-    $result = mysql_query( "select ImageURL from Image where ImageID = " . $image );
-    if ( ! $result )
-    {
-      $error .= "Problem retrieving the image from the database.<BR>";
-      $fatal = true;
-    }
-    else
-    {
-      $row = mysql_fetch_row( $result );
-      if ( ! $row )
-      {
-        $error .= "Problem fetching image row from the database.<BR>";
-        $fatal = true;
-      }
-      else
-      {
-        $image = $row[ 0 ];
-      }
-    }
-  }
+            if ( ! $row )
+            {
+                $error .= "Problem fetching scheme row from the database.<BR>";
+                $fatal = true;
+            }
+            else
+            {
+                $bgcolorColor   = $row[ 0 ];
+                $textColor      = $row[ 1 ];
+                $linkColor      = $row[ 2 ];
+                $vlinkColor     = $row[ 3 ];
+                $alinkColor     = $row[ 4 ];
+                $background     = $row[ 5 ];
+                $uncreatedLink  = $row[ 6 ];
+                $createdLink    = $row[ 7 ];
+                $backLinkedLink = $row[ 8 ];
 
-  if ( empty( $error ) )
-  {
-    $result = mysql_query( "select TargetEpisodeID, IsBackLink, Description from LinkEditLog " .
-                           "where EpisodeEditLogID = " . $episodeEditLogID . " order by LinkEditLogID" );
-    if ( ! $result )
-    {
-      $error .= "Problem retrieving link edit log from database.<BR>";
-      $fatal = true;
+                $body = "<BODY BGCOLOR=\"" . $bgcolorColor . "\" " .
+                              "TEXT=\""    . $textColor    . "\" " .
+                              "LINK=\""    . $linkColor    . "\" " .
+                              "VLINK=\""   . $vlinkColor   . "\" " .
+                              "ALINK=\""   . $alinkColor   . "\""  .
+                              ( empty( $background ) ? ">" :
+                                                       " BACKGROUND=\"" . $background . "\">" );
+            }
+        }
     }
-  }
 
-  if ( ! empty( $error ) )
-    displayError( $error, $fatal );
+    if (( empty( $error )) && ( $image != 0 ))
+    {
+        $result = mysql_query( "SELECT ImageURL FROM Image WHERE ImageID = " . $image );
+
+        if ( ! $result )
+        {
+            $error .= "Problem retrieving the image from the database.<BR>";
+            $fatal = true;
+        }
+        else
+        {
+            $row = mysql_fetch_row( $result );
+
+            if ( ! $row )
+            {
+                $error .= "Problem fetching image row from the database.<BR>";
+                $fatal = true;
+            }
+            else
+            {
+                $image = $row[ 0 ];
+            }
+        }
+    }
+
+    if ( empty( $error ))
+    {
+        $result = mysql_query( "SELECT TargetEpisodeID, " .
+                                      "IsBackLink, " .
+                                      "Description " .
+                                 "FROM LinkEditLog " .
+                                "WHERE EpisodeEditLogID = " . $episodeEditLogID . " " .
+                                "ORDER BY LinkEditLogID" );
+
+        if ( ! $result )
+        {
+            $error .= "Problem retrieving link edit log from database.<BR>";
+            $fatal = true;
+        }
+    }
+
+    if ( ! empty( $error ))
+    {
+        displayError( $error, $fatal );
+    }
 
 ?>
+
 <HTML><HEAD>
-<TITLE><?php echo( $storyName ); ?>: <?php echo( $title ); ?> [Episode <?php echo( $episode ); ?>] - Edit Log</TITLE>
+<TITLE>
+    <?php echo( $storyName ); ?>: <?php echo( $title ); ?>
+    [Episode <?php echo( $episode ); ?>] - Edit Log
+</TITLE>
 </HEAD><?php echo( $body ); ?>
 
 <CENTER>
@@ -279,84 +329,107 @@ You do not have permission to view this edit log.
 <H2><?php echo( $storyName ); ?> - Episode <?php echo( $episode ); ?> - Edit Log</H2>
 
 <TABLE WIDTH="500">
-  <TR>
-    <TD>
+    <TR>
+        <TD>
 <?php echo( $text ); ?>
+
 <?php
 
-  if ( ! empty( $image ) )
-  {
+    if ( ! empty( $image ))
+    {
 
 ?>
+
 <P>
 <CENTER>
 <IMG SRC="<?php echo( $image ); ?>">
 </CENTER>
+
 <?php
 
-  }
+    }
 
 ?>
+
 <P>
 <OL>
+
 <?php
 
-  for ( $i = 0; $i < mysql_num_rows( $result ); $i++ )
-  {
-    $row = mysql_fetch_row( $result );
+    for ( $i = 0; $i < mysql_num_rows( $result ); $i++ )
+    {
+        $row = mysql_fetch_row( $result );
 
-    $description = $row[ 2 ];
-    $description = htmlentities( $description );
-    $description = strtr( $description, getOptionTranslationTable( ) );
+        $description = $row[ 2 ];
+        $description = htmlentities( $description );
+        $description = strtr( $description, getOptionTranslationTable() );
 
-    if ( $row[ 1 ] == "Y" )
-      $image = $backLinkedLink;
-    else
-      $image = $uncreatedLink;
+        if ( $row[ 1 ] == "Y" )
+        {
+            $image = $backLinkedLink;
+        }
+        else
+        {
+            $image = $uncreatedLink;
+        }
 
 ?>
-<LI><IMG SRC="<?php echo( $image ); ?>"><A HREF="read.php?episode=<?php echo( $row[ 0 ] ); ?>"><?php echo( $description ); ?></A></LI>
+
+<LI>
+    <IMG SRC="<?php echo( $image ); ?>">
+    <A HREF="read.php?episode=<?php echo( $row[ 0 ] ); ?>"><?php echo( $description ); ?></A>
+</LI>
+
 <?php
 
   }
 
 ?>
+
 </OL>
+
 <?php
 
-  if ( $isExtendable == "Y" )
-  {
+    if ( $isExtendable == "Y" )
+    {
 
 ?>
+
 <P>
 <A HREF="create.php?episode=<?php echo( $episode ); ?>&command=Extend">Add New Option</A>
+
 <?php
 
-  }
+    }
 
-  if ( $episode != 1 )
-  {
+    if ( $episode != 1 )
+    {
 
 ?>
+
 <P>
 <A HREF="read.php?episode=<?php echo( $parent ); ?>">Go Back</A>
+
 <?php
 
-  }
+    }
 
-  if ( $linkCount > 1 )
-  {
+    if ( $linkCount > 1 )
+    {
 
 ?>
+
 <P>
 <A HREF="link-trace.php?episode=<?php echo( $episode ); ?>">Display All <?php echo( $linkCount ); ?> Links to this Episode</A>
+
 <?php
 
-  }
+    }
 
 ?>
-    </TD>
-  </TR>
+
+        </TD>
+    </TR>
 </TABLE>
 
 <HR>
