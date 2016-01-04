fe3dcaee878a0a71676886097ce5b95b109a32ae
Jeff Weston <jjweston@gmail.com>
2016-01-03 16:32:50 -0800

    Updated the help text for the database connection fields.

--- www/include/classes/Extend_A_Story/Pages/Install/DatabaseConnectionPage.php
+++ www/include/classes/Extend_A_Story/Pages/Install/DatabaseConnectionPage.php
@@ -141,21 +141,21 @@ class DatabaseConnectionPage extends InstallPage
 
         $databaseHostField = new InputField(
                 "databaseHost", "Host", "text", $databaseHost,
-                "This is the host name for your database. This is typically \"localhost\" unless " .
-                "your database is running on a different server than your web site. If you are " .
+                "This is the host name for your database server. If your database server and " .
+                "your web server are running on the same machine, use \"localhost\". If you are " .
                 "running Extend-A-Story in a shared hosting environment, your hosting provider " .
-                "should provide you with your database host name." );
+                "will provide you with the host name for your database server." );
 
         $databaseUsernameField = new InputField(
                 "databaseUsername", "Username", "text", $databaseUsername,
-                "This is the username that will be used to connect to your database during the " .
-                "installation process. This user will need all permissions to your " .
+                "This is the username that will be used to connect to your database server " .
+                "during the installation process. This user will need all permissions to your " .
                 "Extend-A-Story database." );
 
         $databasePasswordField = new InputField(
                 "databasePassword", "Password", "password", $databasePassword,
-                "This is the password that will be used to connect to your database during the " .
-                "installation process." );
+                "This is the password that will be used to connect to your database server " .
+                "during the installation process." );
 
         $databaseNameField = new InputField(
                 "databaseName", "Database", "text", $databaseName,
