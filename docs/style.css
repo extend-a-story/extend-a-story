303af8bb8820195417fb8c5ba62fda00b9505cd7
Jeff Weston <jjweston@gmail.com>
2017-04-24 15:30:11 -0700

    Updated copyright for 2017.

--- docs/style.css
+++ docs/style.css
@@ -1,7 +1,7 @@
 /*
 
 Extend-A-Story - Interactive, Extendable, Choose Your Own Adventure Story
-Copyright (C) 2002-2016 Jeffrey J. Weston
+Copyright (C) 2002-2017 Jeffrey J. Weston
 
 
 This program is free software; you can redistribute it and/or modify
--- sql/DBMaintenance.sql
+++ sql/DBMaintenance.sql
@@ -1,5 +1,5 @@
 # Extend-A-Story - Interactive, Extendable, Choose Your Own Adventure Story
-# Copyright (C) 2002-2016 Jeffrey J. Weston
+# Copyright (C) 2002-2017 Jeffrey J. Weston
 #
 #
 # This program is free software; you can redistribute it and/or modify
