f299592e06487c2d92c99c45925b0ffb7d2dd125
Jeff Weston <jjweston@gmail.com>
2022-10-18 15:49:46 -0700

    Remove call to get_magic_quotes_gpc() function.

--- www/general.php
+++ www/general.php
@@ -28,11 +28,6 @@ http://www.sir-toby.com/extend-a-story/
 
 function prepareParam( &$param )
 {
-    if ( get_magic_quotes_gpc() == 1 )
-    {
-        $param = stripslashes( $param );
-    }
-
     $param = trim( $param );
 }
 
