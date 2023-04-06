fcb4e585adf639424f0f5f224b6884bbb6241bd1
Jeff Weston <jjweston@gmail.com>
2023-04-05 20:31:45 -0700

    Set language for install pages.

--- www/include/classes/Extend_A_Story/Pages/Install/InstallPage.php
+++ www/include/classes/Extend_A_Story/Pages/Install/InstallPage.php
@@ -95,7 +95,7 @@ abstract class InstallPage
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <link rel="stylesheet" type="text/css" href="style.css">
